d469bdb2f9c8d6b59b3632e7597b02ab55eb668d
xplicit <svg@ngs.ru>
2014-06-14 15:38:49 +0700

    added default value for log

--- src/HyperFastCgi/Configuration/LogConfig.cs
+++ src/HyperFastCgi/Configuration/LogConfig.cs
@@ -8,11 +8,20 @@ namespace HyperFastCgi.Configuration
 	[XmlRoot("log")]
 	public class LogConfig
 	{
+		private LogLevel level = LogLevel.Error;
+		private bool writeToConsole = true;
+
 		[XmlAttribute("level")]
-		public LogLevel Level { get; set; }
+		public LogLevel Level { 
+			get { return level; } 
+			set { level = value; }
+		}
 
 		[XmlAttribute("write-to-console")]
-		public bool WriteToConsole { get; set; }
+		public bool WriteToConsole { 
+			get { return writeToConsole; } 
+			set { writeToConsole = value; }
+		}
 	}
 }
 
