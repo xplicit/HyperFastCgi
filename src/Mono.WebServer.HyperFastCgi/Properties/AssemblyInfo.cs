77929745094348104513065fcad9355643eb1d7f
sergey <sergey@ubuntu.(none)>
2014-06-23 13:18:02 -0700

    fixed issue #12
    Place into the gac proper version of server for .NET 2.0 profile

--- src/Mono.WebServer.HyperFastCgi/Properties/AssemblyInfo.cs
+++ src/Mono.WebServer.HyperFastCgi/Properties/AssemblyInfo.cs
@@ -14,7 +14,12 @@ using System.Runtime.CompilerServices;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("1.0.*")]
+#if NET_2_0
+[assembly: AssemblyVersion ("0.3.2.0")]
+#else
+[assembly: AssemblyVersion ("0.3.4.0")]
+#endif
+
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
