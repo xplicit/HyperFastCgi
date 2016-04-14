e767eac233c41a832cff97f846019beeab9e160c
Sergey Zhukov <svg@ngs.ru>
2016-04-14 21:54:01 +0000

    fixed reference to script

--- samples/ubuntu-startup/init.d/install-service.sh
+++ samples/ubuntu-startup/init.d/install-service.sh
@@ -2,5 +2,5 @@
 #before run place your server config script into /ect/hyperfastcgi/hfc.config file
 sudo mkdir -p /var/log/hyperfastcgi
 sudo chown www-data:www-data /var/log/hyperfastcgi
-sudo cp init.d/hyperfastcgi4 /etc/init.d/hyperfastcgi4
+sudo cp hyperfastcgi4 /etc/init.d/hyperfastcgi4
 sudo update-rc.d hyperfastcgi4 start 90 2 3 4 5 . stop 20 0 1 6 .
